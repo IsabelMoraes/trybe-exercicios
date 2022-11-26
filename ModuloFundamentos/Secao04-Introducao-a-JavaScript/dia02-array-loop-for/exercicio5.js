88b2a8a250e277d71a85d5e261cd8eb8be4852d9
im03moraes@gmail.com <im03moraes@gmail.com>
2022-11-26 02:21:42 +0000

    exercicios

--- ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia02-array-loop-for/exercicio5.js
+++ ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia02-array-loop-for/exercicio5.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+/*
 let result= 0;
 
 for (let index = 0; index < numbers.length; index +=1 ) {
@@ -9,11 +9,19 @@ for (let index = 0; index < numbers.length; index +=1 ) {
 let mean = result / numbers.length;
 console.log(mean);
 
+
+
+
 if (mean <= 20) {
     console.log ('valor maior que 20');
 }  else {
     console.log ('valor menor ou igual a 20');
 }
+
+
+
+
+
 
 
 let higherNumber = numbers[0];
@@ -23,6 +31,10 @@ for (let index = 1; index < numbers.length; index +=1){
  }
 }  
 console.log(higherNumber);
+
+
+
+
 
 
 let totalEven=0;
@@ -39,3 +51,31 @@ let totalEven=0;
 }
 
 
+
+
+
+
+let minorNumber = numbers[0];
+for (let index = 1; index < numbers.length; index +=1){
+ if (numbers[index] < minorNumber) {
+    minorNumber= numbers[index];
+ }
+}  
+console.log(minorNumber);
+
+
+
+*/
+
+
+let myarray = []
+ for ( myarray = 1; myarray <=25; myarray +=1){
+    console.log (myarray);
+ }
+
+
+
+
+for (let index = 0; index < myarray.length; index +=1) {
+    console.log(myarray[index] / 2);
+}
