f3408750685a5b9062d5c09324b950c711733664
im03moraes@gmail.com <im03moraes@gmail.com>
2022-11-29 22:35:24 +0000

    metade exercicio 3

--- ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia03-javascript-logica-de-programacao/exercicio01.js
+++ ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia03-javascript-logica-de-programacao/exercicio01.js
@@ -1,10 +1,12 @@
 /* Exercício 1: Escreva um código que reproduza o resultado de uma fatoração*/
 
-/*  let number = 1;
+    let number = 1;
    for (let counter=10; counter>0;counter-=1) {
     number*=counter;
    }
-   console.log(number); */
+   console.log(number);
+
+
 
 
 /* Um algoritmo com for que reproduza o inverso da palavra definida*/   
@@ -16,3 +18,21 @@ let reverse = '';
         reverse += word[word.length-1-index];
     }
     console.log(reverse);
+
+
+
+/*Imprimir a maior palavra e a menor*/
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = '';
+let menor = '';
+
+for (index=0; index<array.length; index+=1){
+
+    let tamanho=array[index].length;
+    console.log(tamanho);
+    /* ate aqui consegui dizer o tamanho das palavras na array. falta comparar e mostrar na tela*/
+
+}
+
