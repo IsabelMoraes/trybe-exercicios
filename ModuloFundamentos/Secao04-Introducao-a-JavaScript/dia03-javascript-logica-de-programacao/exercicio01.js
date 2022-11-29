f2b44ffd14f711bd28c8acdcf494330a75df8afa
im03moraes@gmail.com <im03moraes@gmail.com>
2022-11-29 19:12:08 +0000

    resolvendo exercicio 1

---
+++ ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia03-javascript-logica-de-programacao/exercicio01.js
@@ -0,0 +1,7 @@
+/* Exercício 1: Escreva um código que reproduza o resultado de uma fatoração*/
+
+  let number = 1;
+   for (let counter=10; counter>0;counter-=1) {
+    number*=counter;
+   }
+   console.log(number);
