7cdab3cc83dd079bff57d3ccb7050d4d4c1dda6f
im03moraes@gmail.com <im03moraes@gmail.com>
2022-11-29 22:21:01 +0000

    exercicio 2

--- ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia03-javascript-logica-de-programacao/exercicio01.js
+++ ModuloFundamentos/Secao04-Introducao-a-JavaScript/dia03-javascript-logica-de-programacao/exercicio01.js
@@ -1,7 +1,18 @@
 /* Exercício 1: Escreva um código que reproduza o resultado de uma fatoração*/
 
-  let number = 1;
+/*  let number = 1;
    for (let counter=10; counter>0;counter-=1) {
     number*=counter;
    }
-   console.log(number);
+   console.log(number); */
+
+
+/* Um algoritmo com for que reproduza o inverso da palavra definida*/   
+
+let word='samba';
+let reverse = '';
+
+   for( let index=0; index < word.length; index+=1){
+        reverse += word[word.length-1-index];
+    }
+    console.log(reverse);
